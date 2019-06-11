9b8eb821341bf9a2d37522cf393bea6f67257911
Baishang <42727348+bai-shang@users.noreply.github.com>
2019-06-11 11:17:22 +0800

    Update dowload_synth90k_and_create_tfrecord.sh

--- data/dowload_synth90k_and_create_tfrecord.sh
+++ data/dowload_synth90k_and_create_tfrecord.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+COUNT=50000
+
 # download synth90k
 wget -c http://www.robots.ox.ac.uk/~vgg/data/text/mjsynth.tar.gz
 tar -xzvf mjsynth.tar.gz
 
 # create synth90k image_list.txt
 # xxx/xxx/1234_AbCd_8888.jpg
-find ./mnt/ | xargs ls -d | grep jpg > image_list.txt
+find ./mnt/ | xargs ls -d | grep jpg > image_list_all.txt
+
+# use some of data to train and eval
+cat image_list_all.txt | head -n ${COUNT} > image_list.txt
 
 # create tfrecord
 python create_synth90k_tfrecord.py --image_dir '' --anno_file ./image_list.txt --char_map_json_file ../char_map/char_map.json
